1dfff33a2a22948b2002124df8f82db7aac7963c
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-11 11:04:18 +0200

    add npm install -g firebase-tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app2/pages/logIn.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
+    home: LoginScreen(),
     );
   }
 }
